9123987e6e615817bc3fbd70b22caf40bfe00e41
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-09-29 08:35:52 -0300

    Arrumando erro

--- index.php
+++ index.php
@@ -3,6 +3,22 @@ include("conecta.php");
 $conexao = conectar();
 $sql= "SELECT * FROM livro";
 $res = mysqli_query($conexao, $sql);
+
+$sql2 = "SELECT * FROM acessorio";
+$resultado = mysqli_query($conexao, $sql2);
+
+function recortarText($texto){
+    $palavras = explode(' ', $texto);
+    $recorte = array_slice($palavras, 0, 15);
+    $sinopse = implode(' ', $recorte);
+    return $sinopse;
+}
+
+function mascaraDinheiro($valor) {
+    $valor = number_format($valor, 2, ',', '.');
+    return $valor;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -182,46 +198,8 @@ $res = mysqli_query($conexao, $sql);
         </div>
     </main>
     <footer>
-
+    <!--Pega do pc-->
     </footer>
-    <h1>Nossos Livros</h1>
-    <a href="livros.php">Livros disponiveis</a>
-    <a href="autores.php">Autores recomendados</a>
-    <a href="cad_livro.php">Cadastrar</a>
-<?php
-include("conecta.php");
-$conexao = conectar();
-$sql= "SELECT * FROM livro";
-$res = mysqli_query($conexao, $sql);
-?>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="shortcut icon" href="./assets/favicon.png" type="image/x-icon">
-    <title>Livros</title>
-</head>
-<body>
-    <header>
-
-    </header>
-    <main>
-
-    </main>
-    <footer>
-
-    <h2>Acessorios da nossa loja<h2>
-        <a href="acessorios.php">Acessorios disponiveis</a>
-    <table>
-        <tr>
-            <th>Livros</th>
-            <th>Autor</th>
-        </tr>
-    </table>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
